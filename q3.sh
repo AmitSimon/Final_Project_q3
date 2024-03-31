8fc855286b9074aca2bbf563912c8af5c9b3dd89
Amit Simon <amitsimon1999@gmail.com>
2024-04-01 02:08:55 +0300

    Initial commit.

--- q3.sh
+++ q3.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-# קלט מהמשתמש
+#input
 read -p "Enter a 5-letter word: " word
 read -p "Enter the colors of the letters (g for green, y for yellow, s for gray): " colors
+
+# check if there are 5 characters
+if [[ ${#word} -ne 5 || ${#colors} -ne 5 ]]; then
+    echo "Error: Word and colors must be 5 characters long."
+    exit 1
+fi
